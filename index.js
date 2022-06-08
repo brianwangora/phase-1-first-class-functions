381f97296a9b5306e67220301205d01d577312d3
Brian Wangora <brianwangora@gmail.com>
2022-06-08 13:39:49 +0300

    tests passed

---
+++ index.js
@@ -0,0 +1,22 @@
+const chai = ("chai");
+const spies = ("chai-spies");
+chai.use(spies);
+
+function receivesAFunction (callback) {
+    callback()
+    const spy = () => chai.spy()
+    return spy
+}
+receivesAFunction(spy)
+
+function returnsANamedFunction () {
+    const named = () => {}
+    return named
+}
+
+function returnsAnAnonymousFunction () {
+  return  (function (){
+
+    });
+    
+}
